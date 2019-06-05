ae506e7d10aec185bc191e501754e9d5b7184103
Josh Doan <joshd@TagPay.com>
2019-06-05 10:16:01 -0700

    conditional render to select game piece

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -6,41 +6,80 @@ const Board = class Board extends React.Component {
         super(props);
         this.state = {
             move: 0,
-            selectedPiece: '',
-            turn: ''
+            playerPiece: '',
+            computerPiece: '',
+            boxes: {
+                topLeft: 'O',
+                topMid: '',
+                topRight: '',
+                midLeft: '',
+                center: '',
+                midRight: '',
+                botLeft: '',
+                botMid: '',
+                botRight: ''
+            },
+            computerTurn: false,
+            gameActive: true
         };
     }
 
     onClick = (box) => {
-        this.setState({})
+        if (!this.state.boxes[box]) {
+            this.setState(prevState => ({
+                boxes: {
+                    ...prevState.boxes,
+                    [box]: this.state.playerPiece
+                },
+                computerTurn: true
+            }))
+        }
+    }
+
+    selectPiece = (piece) => {
+        (piece === 'X') ? this.setState({ playerPiece: 'X', computerPiece: 'O' }) : this.setState({ playerPiece: 'O', computerPiece: 'X' })
     }
 
     render() {
-        return (
-            <div className={styles.container}>
-                <div className={styles.header}>
-                    <h1>Tic-Tac-Toe</h1>
-                </div>
-                <div className={styles.board}>
-                    <div className={styles.top}>
-                        <div onClick={() => this.onClick('topLeft')} className={styles.topLeft}>{this.state.topLeft}</div>
-                        <div onClick={this.onClick} className={styles.topMid}></div>
-                        <div onClick={this.onClick} className={styles.topRight}></div>
+        if (!this.state.playerPiece && !this.state.computerPiece) {
+            return (
+                <div className={styles.container}>
+                    <div className={styles.header}>
+                        Select a Piece
                     </div>
-                    <div className={styles.mid}>
-                        <div onClick={this.onClick} className={styles.midLeft}></div>
-                        <div onClick={this.onClick} className={styles.center}></div>
-                        <div onClick={this.onClick} className={styles.midRight}></div>
-                    </div>
-                    <div className={styles.bot}>
-                        <div onClick={this.onClick} className={styles.botLeft}></div>
-                        <div onClick={this.onClick} className={styles.botMid}></div>
-                        <div onClick={this.onClick} className={styles.botRight}></div>
+                    <div>
+                        <span onClick={() => this.selectPiece('X')} className={styles.piece}>X</span>
+                        <span onClick={() => this.selectPiece('O')} className={styles.piece}>O</span>
                     </div>
                 </div>
-                {/* <button className={styles.btn} type="button">Lock In</button> */}
-            </div>
-        );
+            );
+        }
+        else {
+            return (
+                <div className={styles.container}>
+                    <div className={styles.header}>
+                        Tic-Tac-Toe
+                    </div>
+                    <div className={styles.board}>
+                        <div className={styles.top}>
+                            <div disabled={this.state.computerTurn} onClick={() => this.onClick('topLeft')} className={styles.topLeft}>{this.state.boxes.topLeft}</div>
+                            <div onClick={() => this.onClick('topMid')} className={styles.topMid}>{this.state.boxes.topMid}</div>
+                            <div onClick={() => this.onClick('topRight')} className={styles.topRight}>{this.state.boxes.topRight}</div>
+                        </div>
+                        < div className={styles.mid}>
+                            <div onClick={() => this.onClick('midLeft')} className={styles.midLeft}>{this.state.boxes.midLeft}</div>
+                            <div onClick={() => this.onClick('center')} className={styles.center}>{this.state.boxes.center}</div>
+                            <div onClick={() => this.onClick('midRight')} className={styles.midRight}>{this.state.boxes.midRight}</div>
+                        </div>
+                        <div className={styles.bot}>
+                            <div onClick={() => this.onClick('botLeft')} className={styles.botLeft}> {this.state.boxes.botLeft}</div>
+                            <div onClick={() => this.onClick('botMid')} className={styles.botMid}>{this.state.boxes.botMid}</div>
+                            <div onClick={() => this.onClick('botRight')} className={styles.botRight}>{this.state.boxes.botRight}</div>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
